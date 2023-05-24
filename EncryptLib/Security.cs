c3471be67f0c3c08ef51f2d9ce0f05df0c03780a
DragonicDefson <bradley-methorst@outlook.com>
2023-05-24 07:55:59 +0200

    Uploaded initial files.

---
+++ EncryptLib/Security.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.Configuration;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using System;
+using System.Configuration;
+using System.Text;
+
+/*
+                                                        @@@                                                                     
+                                                      @@@@@@@@@@@@@@@@@@@@@@,                                                   
+                                                  %@@@@@@@@,           #@@@@@@@@                                                
+                                               %@@@@@@@@@@@&                 @@@@@@    #                                        
+                                             @@@@@@@@@@@@@@@                    @@@@@@                                          
+                                            @@@@*@@@@@@@@@@@                   @@@@@@@@                                         
+                                          @@@@@  @@@@@@@@@@@@@           %@@@@@@@   @@@@&                                       
+                                         %@@@@  @@@@@@@@@@@@@@@@   @@@@@@@@@@@       @@@@%                                      
+                                         @@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@          @@@@                                      
+                                        &@@@        #@@@@@@@@@@@@@@@@@@@@@            @@@@@                                     
+                                        @@@@          @@@@@@@@@@@@@@@@@@               @@@@                                     
+                                        @@@@      (  @@@@@@@@@@@@@@@@@             &@&.@@@@                                     
+                                         @@@@     @@@@@@@@@@@@@@@@@@@            @@@@@@@@@                                      
+                                         @@@@    @@@@@@@  #@@@@@@@@@@@@@    @@/      @@@@@                                      
+                                          @@@@    @@@@@@          #@@@@@@@          @@@@@                                       
+                                           @@@@@    @@@@@          @@@@@@@@@       @@@@@                                        
+                                             @@@@@      @@@    &@  @@@@@@@@@@    @@@@@                                          
+                                               @@@@@&     @@       @@@     @@@@@@@@@                                            
+                                                 %@@@@@@@   %@     @      @@@@@@@&                                              
+                                                 #@  @@@@@@@@@@@@@@@@@@@@@@@@@  *@                                              
+                                            @@@@           %@@@@@@@@@@@#                                                                                                                                                                                                                                                                                                                  
+*/
+
+
+namespace EncryptLib
+{
+    public class Security
+    {
+        private static byte[] PrivateKey;
+        private static string Algorithm;
+        private static int IVLength;
+        private static char IVSeperator;
+
+        public static string AES256Encrypt(string data)
+        {
+            IBufferedCipher cipherEncrypt = CipherUtilities.GetCipher(Algorithm);
+            byte[] vector = GenHMAC();
+            ICipherParameters parameters = new ParametersWithIV(new KeyParameter(PrivateKey), vector);
+            cipherEncrypt.Init(true, parameters);
+            byte[] ciphered = cipherEncrypt.DoFinal(Encoding.UTF8.GetBytes(data));
+            return $"{Convert.ToBase64String(ciphered)}{IVSeperator}{Convert.ToBase64String(vector)}";
+        }
+
+        public static string AES256Decrypt(string data)
+        {
+            IBufferedCipher cipherDecrypt = CipherUtilities.GetCipher(Algorithm);
+            byte[] vector = Convert.FromBase64String(data.Split(IVSeperator)[1]);
+            byte[] ciphered = Convert.FromBase64String(data.Split(IVSeperator)[0]);
+            ICipherParameters parameters = new ParametersWithIV(new KeyParameter(PrivateKey), vector);
+            cipherDecrypt.Init(false, parameters);
+            return Encoding.UTF8.GetString(cipherDecrypt.DoFinal(ciphered));
+        }
+
+        private static byte[] GenHMAC()
+        {
+            return new SecureRandom().GenerateSeed(IVLength);
+        }
+
+        public static void SetWebConfig (IConfiguration configuration)
+        {
+            PrivateKey = Encoding.UTF8.GetBytes(configuration["EncryptLib.PrivateKey"]);
+            Algorithm = configuration["EncryptLib.Algorithm"];
+            IVLength = int.Parse(configuration["EncryptLib.IVLength"]);
+            IVSeperator = configuration["EncryptLib.IVSeperator"].ToCharArray()[0];
+        }
+
+        public static void SetAppConfig()
+        {
+            PrivateKey = Encoding.UTF8.GetBytes(ConfigurationManager.AppSettings.Get("EncryptLib.PrivateKey"));
+            Algorithm = ConfigurationManager.AppSettings.Get("EncryptLib.Algorithm");
+            IVLength = int.Parse(ConfigurationManager.AppSettings.Get("EncryptLib.IVLength"));
+            IVSeperator = ConfigurationManager.AppSettings.Get("EncryptLib.IVSeperator").ToCharArray()[0];
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # EncryptLib
 Easy encryption library
+
+- Written in C# for an old-colleague of mine requiring an encryption implementation for his game server framework.
+- Designed for AES-256.
+- Nothing too fancy, nothing too shabby.
